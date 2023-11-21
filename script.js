2fb57c0b607e26305f42e32703d39e3fb58af80e
ChidoMunyanyi <119809704+ChidoMunyanyi@users.noreply.github.com>
2023-11-21 20:41:10 +0000

    created calculator class

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+    delete() {
+
+    }
+    appendNumber(number) {
+
+    }
+    chooseOperation(operation) {
+
+    }
+    compute() {
+
+    }
+    updateDisply() {
+
+    }
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
