2503224a9554ab802c62cd9cf1e8752b7b2e36f8
ChidoMunyanyi <119809704+ChidoMunyanyi@users.noreply.github.com>
2023-11-25 12:31:21 +0000

    added functions for buttons

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
     clear() {
         this.currentOperand = ''
@@ -12,15 +13,20 @@ class Calculator {
 
     }
     appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
 
     }
     chooseOperation(operation) {
+        this.operation = operation
+        this.
 
     }
     compute() {
 
     }
-    updateDisply() {
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
 
     }
 }
@@ -33,3 +39,19 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const deleteButton = document.querySelector('[data-delete]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+    })
+})
